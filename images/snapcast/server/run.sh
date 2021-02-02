77c813310d409ce2cc6e9f60914ea034e7cd08e5
atticuss <jonn.callahan@gmail.com>
2021-02-02 08:51:04 -0500

    slightly better snapcast server dockerfile

--- images/snapcast/server/run.sh
+++ images/snapcast/server/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo "init of server.json"
-echo "{}" > /var/lib/snapserver/server.json
-echo "spotify starting"
 snapserver -s "spotify:///librespot?name=Spotify&username=${SPOT_USERNAME}&password=${SPOT_PASSWORD}&devicename=Deck&bitrate=320&killall=false"
